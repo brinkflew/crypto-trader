7ab394b8872c558ed0ca05da683616bc72db4c5c
avs <avs@odoo.com>
2022-08-17 16:04:39 +0200

    [DEBUG]

--- trader/trader.py
+++ trader/trader.py
@@ -40,7 +40,7 @@ class Trader:
                 if not pair.from_coin.enabled or not pair.to_coin.enabled:
                     continue
 
-                logger.over(f"Initializing pair {term.yellow_bold(str(pair))}")
+                logger.info(f"Initializing pair {term.yellow_bold(str(pair))}")
 
                 from_coin_price = self.manager.get_ticker_price(pair.from_coin + self.config.FIAT_SYMBOL)
                 if from_coin_price is None:
